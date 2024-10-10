50ace48a3b6dca4501edd7ff55ac687a96ef3f67
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2024-10-10 17:09:31 -0300

    fix Cannot use 'in' operator to search for 'Array'

--- assets/javascripts/materialize.js
+++ assets/javascripts/materialize.js
@@ -1505,7 +1505,7 @@ $jscomp.iteratorPrototype = function (e) {
 };
 $jscomp.polyfill = function (e, r, p, m) {
   if (r) {
-    p = $jscomp.global;e = e.split(".");for (m = 0; m < e.length - 1; m++) {
+    p = $jscomp.global;e = e.split(".");if(!p) p = new Array();for (m = 0; m < e.length - 1; m++) {
       var u = e[m];u in p || (p[u] = {});p = p[u];
     }e = e[e.length - 1];m = p[e];r = r(m);r != m && null != r && $jscomp.defineProperty(p, e, { configurable: !0, writable: !0, value: r });
   }
